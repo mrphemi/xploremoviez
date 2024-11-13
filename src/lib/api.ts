abd83de48967a0b2ea0fcac8f59d53e1f134543b
Femi <mrlapinho@gmail.com>
2024-11-13 01:54:04 +0000

    remove token from project

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,8 +5,7 @@ const options = {
 	method: "GET",
 	headers: {
 		accept: "application/json",
-		Authorization:
-			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTBjZTc1MzIzYjkzZGU1MzExODQ2NzJhZWViM2JlNiIsIm5iZiI6MTczMTI1NDYzMi44MDI4MzE0LCJzdWIiOiI1Y2EzNzM2YTkyNTE0MTQwY2Y0OThkOTMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.X7AAaCq_ZDJrblgltQeHZ_2Daizbj7CjpHqtzPb_P6Q",
+		Authorization: "Bearer token!",
 	},
 };
 
